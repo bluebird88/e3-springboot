9e2db10b4ff1fe1478b624a50313aa55a337a08d
bluebird88 <bluebird88@139.com>
2018-11-07 16:22:44 +0800

    shell added

---
+++ cmd/order.sh
@@ -0,0 +1,27 @@
+basedir=`pwd`
+app="order"
+runAt="$basedir/../$app"
+
+if [ ! -d  $runAt ]; then
+	mkdir $runAt
+fi
+
+if [ "$1" = "web" ]; then
+	echo "launch $app web app..."
+	cd "e3-$app-$1"
+	mvn spring-boot:run  > $runAt/$1.log 2>&1 &
+	 echo $! > $runAt/pid_$1.txt
+	cd $basedir
+fi
+
+if [ "$1" = "svr" ];then
+	echo "launch $app service app..."
+	cd "e3-$app/e3-$app-service"
+	mvn spring-boot:run  > $runAt/$1.log 2>&1 &
+	 echo $! > $runAt/pid_$1.txt
+	cd $basedir
+fi
+
+if [ "$1" = "" ];then
+	echo "no implemented!"
+fi
