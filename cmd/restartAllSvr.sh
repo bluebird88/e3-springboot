f095bc5d9bdd0a8c137cfc5e50df1b2a72883594
bluebird88 <bluebird88@139.com>
2018-12-04 15:00:33 +0800

    noop

--- cmd/restartAllSvr.sh
+++ cmd/restartAllSvr.sh
@@ -2,7 +2,7 @@ SVR="search sso cart content order manager"
 echo 'to start service:' $SVR
 for x in $SVR
 do
-echo "\t \t start $x ...."
+echo "          start $x ...."
 ./runit.sh $x svr
 sleep 5
 
